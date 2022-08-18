fd565563e1ef11a993a8bb5a4a4902ae1fc5a4ac
Josh Murray <joshmurray@Joshs-MBP.lan>
2022-08-18 16:37:26 -0700

    updated files

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+="
 
 const passwordText = document.getElementById("passwords");
 const length = document.getElementById("length");
 const incNumbers = document.getElementById("numbers");
+const incUpper = document.getElementById("upper");
 const incSymbols = document.getElementById("symbols");
 const generateBtn = document.getElementById("generate");
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", () => {
+  let characters = alphabet;
+  incNumbers.checked ? (characters += numbers) : "";
+  incUpper.checked ? (characters += upper) : "";
+  incSymbols.checked ? (characters += symbols) : "";
+  passwordText.value = generatePassword(length.value, characters);
+});
+
+const generatePassword = (length, characters) => {
+  let passwords = "";
+  for (let i = 0; i < length; i++) {
+    passwords += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return passwords;
+};
+
